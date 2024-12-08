96492821b382433ac07025ab89dc7612d5c05348
Kashish2130 <jainkashish2130@gmail.com>
2024-12-08 19:58:47 +0530

    deleted package.json

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-//const lib = require('./lib.js')
+//const lib = require('./lib.js') //commonjs syntax
 import {sum,diff} from './lib.js';
-import fs from 'fs';
+// =========================================================================================================================
+// UNDERSTANDING THE BASICS
 
-const txt = fs.readFileSync('demo.txt','utf-8')
-console.log(txt)
+// import fs from 'fs';
 
-console.log(sum(2,3),diff(9,3))
+// // const txt = fs.readFileSync('demo.txt','utf-8') //if we would use this fucntion then it would more time than readfile to execute
+// fs.readFile('demo.txt','utf-8',(err, txt)=>{
+//     console.log(txt)
+// });
+// const t1 = performance.now(); //count time it takes for the execution of above readfile function
+
+// console.log(sum(2,3),diff(9,3))
+// const t2 = performance.now() // counts time it takes for the execution of console log
+// console.log(t2-t1); // here it counts diff between the above two
+//===============================================================================================
