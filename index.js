4f5595f0b014096984c56da866563fd7f3efd1d3
Kashish2130 <jainkashish2130@gmail.com>
2024-12-08 19:33:51 +0530

    saving last changes

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //const lib = require('./lib.js')
 import {sum,diff} from './lib.js';
+import fs from 'fs';
+
+const txt = fs.readFileSync('demo.txt','utf-8')
+console.log(txt)
 
 console.log(sum(2,3),diff(9,3))
