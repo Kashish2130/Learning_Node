2bfeeb27679f77ebea1dac2a7bf5c5fc7c78bfbd
Kashish2130 <jainkashish2130@gmail.com>
2024-12-12 15:30:36 +0530

    added switch cases

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const index = fs.readFileSync('index.html','utf-8');
 const data = fs.readFileSync('data.json','utf-8');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    switch(req.url){
+        case '/':
+            res.setHeader('Content-Type','text/html');
+            res.end(index);
+            break;
+        case '/api':
+            res.setHeader('Content-Type', 'application/json');
+            res.end(data);
+            break;
+        default :
+            res.writeHead(404,'not foundd');
+            res.end();
+    }
     console.log('server started')
-    // res.setHeader('Dummy', 'Dummyvalue');
-    res.setHeader('Content-Type', 'application/json');
-    res.end(data);
-
 })
 
 server.listen(8080)
